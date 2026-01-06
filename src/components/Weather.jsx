c37a8e6b47e690944f6f523e26684f2e22f63434
Aditi <aditianand748@gmail.com>
2026-01-06 17:59:34 +0530

    Added weather-data

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,6 +6,7 @@ import extremerainy_icon from '../assets/extremerainy.avif'
 import rainy_icon from '../assets/rainy.jpg'
 import sunny_icon from '../assets/sunny.jpg'
 import weather_icon from '../assets/weather.png'
+import windy_icon from '../assets/windy-icon.png'
 
 const Weather = () => {
   return (
@@ -15,9 +16,24 @@ const Weather = () => {
             <img src={search_icon} alt="" />
         </div>
         <img src={weather_icon} alt="" className='weather-icon'/>
-        <p>16c</p>
-        <p>London</p>
-
+        <p className='temperature'>16c</p>
+        <p className='location'>London</p>
+        <div className="weather-data">
+            <div className="col">
+                <img src={cloudy_icon} alt="" />
+                <div>
+                    <p>91 %</p>
+                    <span>cloudy</span>
+                </div>
+            </div>
+             <div className="col">
+                <img src={windy_icon} alt="" />
+                <div>
+                    <p>36 km/hr</p>
+                    <span>windy</span>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
